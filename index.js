d62b1f0ea254e763fbeafcd8de96f3367be12249
yurdeth <angelvasquez2570@gmail.com>
2024-11-18 02:12:46 -0600

    Configuring port

--- index.js
+++ index.js
@@ -48,7 +48,7 @@ app.get("/api", function (req, res) {
     res.send(html);
 });
 
-const PORT = 3000  || process.env.PORT;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
